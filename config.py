a9594fc2d2fe9a560f1b568f5a082141924c7023
Mikolaj.Chrzan <mikochrzan@interia.pl>
2020-03-05 11:42:35 +0100

    Update

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ robot_credentials = {"chrome_path": r"C:\Program Files (x86)\Google\Chrome\Appli
                      "login_path": r"C:\python\web_scrap\ompt_partners.json",
                      "ompt_output": r"C:\Temp\output\OmptData",
                      "sap_output": 'C:\\Temp\\output\\SAP\\',
-                     "email": "mikolaj.chrzan@unilever.com"}
+                     "email": "x.y@unilever.com"}
